614ba485dd3ffe75c7d5167c1d953c49adb0a5af
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2022-04-15 18:27:59 -0300

    Remoção e animação

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,20 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado com um duplo click");
-        this.remove();    // this está sempre atrelado ao dono do evento
-    });
+tabela.addEventListener("dblclick", function(event){ // quem escuta o evento é a tabela
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+     
 })
+
+//    var alvoEvento = event.target;    // target é o alvo do evento
+//    var paiDoALvo = alvoEvento.parentNode;  //TR = paciente = remover
+
+// pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+//        console.log("Fui clicado com um duplo click");
+//        this.remove();    // this está sempre atrelado ao dono do evento//
+//    });
+//})
